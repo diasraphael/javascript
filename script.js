0e587a32cfc41ef92a5310cc4b5c7073ca3d6758
diasraphael <diasraphael88@gmail.com>
2023-02-10 13:17:05 +0100

    datatypes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let firstName = 'Hello Dias';
-let yearOfBirth = 1988;
+let firstName = 'Hello Dias'; // strings
+let yearOfBirth = 1988; // numbers floating point numbers
+// BigInt(ES2020) larger numbers than number type can hold here
 let age = 34;
+let isMarried = true; // boolean type
 console.log(firstName);
 console.log(yearOfBirth);
 console.log(age);
+
+let value; // value taken by a variable that is not yet defined
+let name = null; // null is also empty value
+// symbol : value that is unique and cannot be changed will not be used by us much
